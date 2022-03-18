5fe56b11149dcb78b902c4b9c32fb53f03ba8cb0
Febin Bellamy <febinbellamy@gmail.com>
2022-03-18 09:32:31 -0400

    Bar graph

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -22,10 +22,11 @@ const ExpenseForm = (props) => {
     e.preventDefault();
 
     const expenseData = {
-      title: enteredTitle,
-      amount: enteredAmount,
-      date: new Date(enteredDate),
-    };
+        title: enteredTitle,
+        amount: +enteredAmount,
+        date: new Date(enteredDate),
+      };
+      
 
     props.onSaveExpenseData(expenseData);
     setEnteredAmount("");
@@ -66,6 +67,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+          <button type="button" onClick={props.onCancel}>Cancel</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
